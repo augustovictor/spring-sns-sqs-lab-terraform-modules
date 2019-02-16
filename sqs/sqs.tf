2ae65b56497443be6d2995210d2464f6dd3d02a9
Victor Augusto <victoraweb@gmail.com>
2019-02-16 18:49:02 -0200

    Sqs fix illegal characters

--- sqs/sqs.tf
+++ sqs/sqs.tf
@@ -1,10 +1,10 @@
 resource "aws_sqs_queue" "sqs_queue" {
   name                      = "${var.sqs_queue_name}"
   
-  delay_seconds             = ${var.sqs_queue_delay_seconds}
-  max_message_size          = ${var.sqs_queue_max_message_size}
-  message_retention_seconds = ${var.sqs_queue_message_retention_seconds}
-  receive_wait_time_seconds = ${var.sqs_queue_receive_wait_time_seconds}
+  delay_seconds             = "${var.sqs_queue_delay_seconds}"
+  max_message_size          = "${var.sqs_queue_max_message_size}"
+  message_retention_seconds = "${var.sqs_queue_message_retention_seconds}"
+  receive_wait_time_seconds = "${var.sqs_queue_receive_wait_time_seconds}"
   redrive_policy            = "${var.sqs_queue_redrive_policy}"
 
   tags = {
