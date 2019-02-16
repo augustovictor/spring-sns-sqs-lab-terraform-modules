2cd87bc87f7b423d61c146b818f628d02f8a87d4
Victor Augusto <victoraweb@gmail.com>
2019-02-16 18:13:42 -0200

    Update attribute

--- sns/sns.tf
+++ sns/sns.tf
@@ -7,15 +7,15 @@ resource "aws_sns_topic" "sns_topic" {
       "defaultHealthyRetryPolicy": {
         "minDelayTarget": ${var.sns_topic_min_delay_target},
         "maxDelayTarget": ${var.sns_topic_max_delay_target},
-        "numRetries": "${var.sns_topic_num_retries}",
-        "numMaxDelayRetries": "${var.sns_topic_num_max_delay_retries}",
-        "numNoDelayRetries": "${var.sns_topic_num_no_delay_retries}",
-        "numMinDelayRetries": "${var.sns_topic_num_min_delay_retries}",
+        "numRetries": ${var.sns_topic_num_retries},
+        "numMaxDelayRetries": ${var.sns_topic_num_max_delay_retries},
+        "numNoDelayRetries": ${var.sns_topic_num_no_delay_retries},
+        "numMinDelayRetries": ${var.sns_topic_num_min_delay_retries},
         "backoffFunction": "${var.sns_topic_backoff_function}"
         },
         "disableSubscriptionOverrides": "${var.sns_topic_disable_subscription_overrides}",
         "defaultThrottlePolicy": {
-          "maxReceivesPerSecond": "${var.sns_topic_max_receives_per_second}"
+          "maxReceivesPerSecond": ${var.sns_topic_max_receives_per_second}
         }
       }
     }
