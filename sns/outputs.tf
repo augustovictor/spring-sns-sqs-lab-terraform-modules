a2886dcfefc3951dbf90cd53c50bfddfc170b5a3
Victor Augusto <victoraweb@gmail.com>
2019-02-16 11:51:47 -0200

    [FEAT] Add sns terraform module

---
+++ sns/outputs.tf
@@ -0,0 +1,11 @@
+output "sns_topic_id" {
+  value = "${aws_sns_topic.sns_topic.id}"
+}
+
+output "sns_topic_arn" {
+  value = "${aws_sns_topic.sns_topic.arn}"
+}
+
+output "sns_topic_name" {
+  value = "${aws_sns_topic.sns_topic.name}"
+}
---
+++ sns/sns.tf
@@ -0,0 +1,23 @@
+resource "aws_sns_topic" "sns_topic" {
+  name = "${var.sns_topic_name}"
+  
+  delivery_policy = <<EOF
+  {
+    "http": {
+      "defaultHealthyRetryPolicy": {
+        "minDelayTarget": "${var.sns_topic_min_delay_target}",
+        "maxDelayTarget": "${var.sns_topic_max_delay_target}",
+        "numRetries": "${var.sns_topic_num_retries}",
+        "numMaxDelayRetries": "${var.sns_topic_num_max_delay_retries}",
+        "numNoDelayRetries": "${var.sns_topic_num_no_delay_retries}",
+        "numMinDelayRetries": "${var.sns_topic_num_min_delay_retries}",
+        "backoffFunction": "${var.sns_topic_backoff_function}"
+        },
+        "disableSubscriptionOverrides": "${var.sns_topic_disable_subscription_overrides}",
+        "defaultThrottlePolicy": {
+          "maxReceivesPerSecond": "${var.sns_topic_max_receives_per_second}"
+        }
+      }
+    }
+    EOF
+}
